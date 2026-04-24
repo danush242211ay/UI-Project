426815e63adadc0d359a7a78c0458c513903eb6a
danush242211ay <danush8747@gmail.com>
2026-04-24 11:38:33 +0530

    trying

---
+++ ui-project/src/components/section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between bg-white py-6 px-16'>
+        <h3 className='bg-black text-white'>target audience</h3>
+        <h3>digital banking partner</h3>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ ui-project/src/components/section1/Section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './navbar'
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full bg-amber-50'>   
+    </div>
+  )
+}
+
+export default Section1
