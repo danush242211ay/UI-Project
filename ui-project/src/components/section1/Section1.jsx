456f4b8aa3277fcb45c8364b42ee9597b9d16c31
danush242211ay <danush8747@gmail.com>
2026-04-26 09:23:01 +0530

    rendering problem

--- ui-project/src/components/section1/Section1.jsx
+++ ui-project/src/components/section1/Section1.jsx
@@ -3,7 +3,8 @@ import Navbar from './navbar'
 
 const Section1 = () => {
   return (
-    <div className='h-screen w-full bg-amber-50'>   
+    <div className='h-screen w-full bg-amber-950'>   
+      <h1></h1>
     </div>
   )
 }
